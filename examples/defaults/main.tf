8adc1dd9fa78fe8f398cbe17b619d2e524c55ee5
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2020-10-08 17:23:26 +0100

    adding aws backend role

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -3,6 +3,6 @@ provider "vault" {
   token = "root"
 }
 
-module "tc1_defaults" {
-  source = "github.com/devops-rob/terraform-vault-secrets-engines"
+module "example_defaults" {
+  source = "../../"
 }
