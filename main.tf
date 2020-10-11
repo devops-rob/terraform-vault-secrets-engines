4b055b5be2dbceb94e8c31840ecb2849af86848d
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2020-10-11 17:43:39 +0100

    rabbitq backend role

--- main.tf
+++ main.tf
@@ -122,3 +122,31 @@ resource "vault_consul_secret_backend_role" "consul" {
   ttl     = var.consul_default_lease
   max_ttl = var.consul_max_lease
 }
+
+resource "vault_rabbitmq_secret_backend" "rabbitmq" {
+  count = contains(var.secrets_engines, "rabbitmq") ? 1 : 0
+  path  = "rabbitmq"
+
+  connection_uri    = var.rabbitmq_uri
+  password          = var.rabbitmq_password
+  username          = var.rabbitmq_username
+  verify_connection = var.rabbitmq_verify_connection
+
+  default_lease_ttl_seconds = var.rabbitmq_default_ttl
+  max_lease_ttl_seconds     = var.rabbitmq_maximum_ttl
+}
+
+resource "vault_rabbitmq_secret_backend_role" "rabbitmq" {
+  count   = contains(var.secrets_engines, "rabbitmq") ? 1 : 0
+  backend = vault_rabbitmq_secret_backend.rabbitmq[count.index].path
+  name    = var.rabbitmq_backend_role_name
+
+  vhost {
+    configure = var.rabbitmq_configure_permissions
+    host      = var.rabbitmq_vhost
+    read      = var.rabbitmq_read_permissions
+    write     = var.rabbitmq_write_permissions
+  }
+
+  tags = var.rabbitmq_tags
+}
